85edd88492650f21168296135de0b6f88111fba5
Edwin Scharfe <scharfe.edwin@gmail.com>
2016-06-14 22:36:53 +0200

    Projekt started

---
+++ src/DataGraph.java
@@ -0,0 +1,11 @@
+import java.awt.*;
+
+/**
+ * Created by Edwin on 13/06/2016.
+ */
+public class DataGraph extends Panel {
+    static void main(String[] args){
+        System.out.println("waddup ma niggnogg?!");
+    }
+
+}
