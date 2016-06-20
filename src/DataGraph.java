d42cda980ff0baf9f6bf66affa62512e4749dc44
Edwin Scharfe <scharfe.edwin@gmail.com>
2016-06-20 14:08:07 +0200

    push so i can pull

--- src/DataGraph.java
+++ src/DataGraph.java
@@ -1,12 +1,22 @@
 import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 /**
  * Created by Edwin on 13/06/2016.
  */
 public class DataGraph extends Panel {
     static void main(String[] args){
-        System.out.println("waddup ma niggnogg?!");
-        System.exit(0);
+        Frame frame = new Frame();
+        frame.setLayout(new GridBagLayout());
+        frame.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent windowEvent) {
+                super.windowClosing(windowEvent);
+                System.exit(0);
+            }
+        });
     }
+
 
 }
